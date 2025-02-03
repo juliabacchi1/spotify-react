d3d53d8ead27fc2e02b11abd4fd68fc5276c5465
Júlia Bacchi <134342345+juliabacchi1@users.noreply.github.com>
2025-02-03 16:46:08 -0300

    First commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Header.css";
+import smallLeft from "../../assets/icons/small-left.png";
+import smallRight from "../../assets/icons/small-right.png";
+import search from "../../assets/icons/search.png";
+
+const Header = () => {
+  return (
+    <nav className="header__navigation">
+      <div className="navigation">
+        <button className="arrow-left">
+          <img src={smallLeft} alt="Arrow left" />
+        </button>
+        <button className="arrow-right">
+          <img src={smallRight} alt="Arrow right" />
+        </button>
+      </div>
+      <div className="header__search">
+        <img src={search} alt="Icon search" />
+        <input
+          id="search-input"
+          type="text"
+          maxlength="800"
+          autocorrect="off"
+          autocapitalize="off"
+          spellcheck="false"
+          placeholder="O que você quer ouvir?"
+        />
+      </div>
+      <div className="header__login">
+        <button className="subscribe">Inscreva-se</button>
+        <button className="login">Entrar</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
